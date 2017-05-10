476df3f2758a30f6d4f79898dd474384694c7223
risenanti <risenanti@gmail.com>
2017-05-10 03:26:38 -0500

    Initial

---
+++ Debug/src/floyds.d
@@ -0,0 +1,6 @@
+src/floyds.o: ../src/floyds.c /usr/lib/openmpi/include/mpi.h \
+ /usr/lib/openmpi/include/mpi_portable_platform.h
+
+/usr/lib/openmpi/include/mpi.h:
+
+/usr/lib/openmpi/include/mpi_portable_platform.h:
